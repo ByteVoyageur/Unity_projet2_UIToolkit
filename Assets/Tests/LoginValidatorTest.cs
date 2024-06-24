c5a1b1bc69cc5e4b7a95a97cd37f249244764364
Xiaosong <bixiaosong@gmail.com>
2024-06-24 14:47:32 +0200

    Added FramLogin and readjusted visual elements to fit the Samsung S21 screen

--- Assets/Tests/LoginValidatorTest.cs
+++ Assets/Tests/LoginValidatorTest.cs
@@ -7,60 +7,38 @@ using UnityEngine.SceneManagement;
 
 public class LoginValidatorTest
 {
-    // test empty case
+    // Test case for empty fields
     [Test]
     public IEnumerator LoginValidator_EmptyFields_ShowErrorMessages()
     {
+        // Load the scene
         SceneManager.LoadScene("UI");  
         yield return null;  
 
+        // Get the UIDocument
         var uiDocument = GameObject.FindObjectOfType<UIDocument>();
         Assert.IsNotNull(uiDocument, "UIDocument not found");
 
+        // Get the UI elements
         var emailField = uiDocument.rootVisualElement.Q<TextField>("InputEmailText");
         var passwordField = uiDocument.rootVisualElement.Q<TextField>("inputPasswordText");
         var emailErrorMessage = uiDocument.rootVisualElement.Q<Label>("InfoHintEmail");
         var passwordErrorMessage = uiDocument.rootVisualElement.Q<Label>("infoHintPassword");
         var loginButton = uiDocument.rootVisualElement.Q<Button>("buttonSubmit");
 
+        // Set empty values
         emailField.value = "";
         passwordField.value = "";
 
+        // Simulate button click
         using (var evt = new NavigationSubmitEvent() { target = loginButton })
         {
             loginButton.SendEvent(evt);
         }
         yield return null;  
 
+        // Assert error messages
         Assert.AreEqual("Please enter email.", emailErrorMessage.text);
         Assert.AreEqual("Please enter password.", passwordErrorMessage.text);
-    }
-
-    [Test]
-    public IEnumerator LoginValidator_NonEmptyFields_ShowSuccessMessage()
-    {
-        SceneManager.LoadScene("UI");  
-        yield return null;  
-
-        var uiDocument = GameObject.FindObjectOfType<UIDocument>();
-        Assert.IsNotNull(uiDocument, "UIDocument not found");
-
-        var emailField = uiDocument.rootVisualElement.Q<TextField>("InputEmailText");
-        var passwordField = uiDocument.rootVisualElement.Q<TextField>("inputPasswordText");
-        var emailErrorMessage = uiDocument.rootVisualElement.Q<Label>("InfoHintEmail");
-        var passwordErrorMessage = uiDocument.rootVisualElement.Q<Label>("infoHintPassword");
-        var loginButton = uiDocument.rootVisualElement.Q<Button>("buttonSubmit");
-
-        emailField.value = "test@example.com";
-        passwordField.value = "password";
-
-        using (var evt = new NavigationSubmitEvent() { target = loginButton })
-        {
-            loginButton.SendEvent(evt);
-        }
-        yield return null;  
-
-        Assert.AreEqual("Login successful", emailErrorMessage.text);
-        Assert.AreEqual("Login successful", passwordErrorMessage.text);
     }
 }
